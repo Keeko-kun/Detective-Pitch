457c6b7ec571a8d34493f9dc5d85b737f8ccb7ec
Dylan de Wolff <jaxwolff@avans.nl>
2018-05-19 16:02:53 +0200

    First step towards sentence generation.

---
+++ Assets/Scripts/textStuff/Sentence.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Sentence : MonoBehaviour {
+
+    public int sID;
+    public string sContent;
+
+    public Sentence(int id)
+    {
+        sID = id;
+    }
+
+    public void fillSentence(string content)
+    {
+        sContent = content;
+    }
+}
---
+++ Assets/Scripts/textStuff/Text.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Text : MonoBehaviour {
+
+    public int tID;
+    public List<Sentence> tSentences;
+
+    public Text(int id)
+    {
+        tID = id;
+    }
+
+    public void fillSentence(Sentence sentence)
+    {
+        tSentences.Add(sentence);
+    }
+}
