58059e8b6848564c6cf039149a0452fd62b09828
Dylan de Wolff <jaxwolff@avans.nl>
2018-05-19 20:08:58 +0200

    Attempt at doing face recognition stuff

---
+++ Assets/Scripts/ScoreStuff/ScoreManager.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScoreManager : MonoBehaviour {
+
+    int score;
+    Transform player;
+    PlayerEmotions playerEmotions;
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void GetFace(PlayerEmotions currentEmotion)
+    {
+        playerEmotions = player.GetComponent<PlayerEmotions>();
+        float dominantEmotion = Mathf.Max(playerEmotions.currentAnger, playerEmotions.currentDisgust, playerEmotions.currentEyeClosure,
+                                       playerEmotions.currentSmile, playerEmotions.currentSurprise, playerEmotions.currentBrowRaise,
+                                       playerEmotions.currentChinRaise, playerEmotions.currentSadness);
+
+        if (playerEmotions.currentAnger == dominantEmotion)
+        {
+
+        }
+    }
+}
