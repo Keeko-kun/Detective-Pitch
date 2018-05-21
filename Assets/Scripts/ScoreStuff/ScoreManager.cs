7eb7ee85bf47aebbc4c96bc99229a71353a41a14
Dylan de Wolff <jaxwolff@avans.nl>
2018-05-21 20:01:23 +0200

    Attempt at gaining score from current emotion.

--- Assets/Scripts/ScoreStuff/ScoreManager.cs
+++ Assets/Scripts/ScoreStuff/ScoreManager.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class ScoreManager : MonoBehaviour {
 
     int score;
+    int desiredEmotion;
     Transform player;
     PlayerEmotions playerEmotions;
 
@@ -20,14 +21,43 @@ public class ScoreManager : MonoBehaviour {
 
     void GetFace(PlayerEmotions currentEmotion)
     {
+        player = GameObject.FindGameObjectWithTag("Player").transform;
         playerEmotions = player.GetComponent<PlayerEmotions>();
         float dominantEmotion = Mathf.Max(playerEmotions.currentAnger, playerEmotions.currentDisgust, playerEmotions.currentEyeClosure,
                                        playerEmotions.currentSmile, playerEmotions.currentSurprise, playerEmotions.currentBrowRaise,
                                        playerEmotions.currentChinRaise, playerEmotions.currentSadness);
 
-        if (playerEmotions.currentAnger == dominantEmotion)
+        if (playerEmotions.currentAnger == dominantEmotion && desiredEmotion == 1)
         {
-
+            score++;
+        }
+        else if (playerEmotions.currentDisgust == dominantEmotion && desiredEmotion == 2)
+        {
+            score++;
+        }
+        else if (playerEmotions.currentEyeClosure == dominantEmotion && desiredEmotion == 3)
+        {
+            score++;
+        }
+        else if (playerEmotions.currentSmile == dominantEmotion && desiredEmotion == 4)
+        {
+            score++;
+        }
+        else if (playerEmotions.currentSurprise == dominantEmotion && desiredEmotion == 5)
+        {
+            score++;
+        }
+        else if (playerEmotions.currentBrowRaise == dominantEmotion && desiredEmotion == 6)
+        {
+            score++;
+        }
+        else if (playerEmotions.currentChinRaise == dominantEmotion && desiredEmotion == 7)
+        {
+            score++;
+        }
+        else if (playerEmotions.currentSadness == dominantEmotion && desiredEmotion == 8)
+        {
+            score++;
         }
     }
 }
