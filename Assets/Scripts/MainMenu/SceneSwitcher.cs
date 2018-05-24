16fca64ada01a6848631e5cb21d846349ab1aa78
Sjors Scholten <s.scholten@student.fhict.nl>
2018-05-24 11:01:32 +0200

    added radio buttons for selecting gamemode

---
+++ Assets/Scripts/MainMenu/SceneSwitcher.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class SceneSwitcher : MonoBehaviour {
+    public string StartingGame = "";
+    public Toggle radioButton;
+    public ToggleGroup radioGroup;
+    public List<GameMode> Modes = new List<GameMode>();
+
+    private void Start() {
+        InitializeToggles();
+    }
+
+    private void InitializeToggles() {
+        radioButton.group = radioGroup;
+        for (int g = 0; g < Modes.Count; g++) {
+            
+        }
+    }
+
+    public void StartGame() { SceneManager.LoadScene(StartingGame); }
+    public void ExitApplication() { Application.Quit(); }
+
+}
+
+public struct GameMode {
+    public string Name;
+    public string SceneName;
+}
