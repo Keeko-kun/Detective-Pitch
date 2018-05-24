77480f3761a462ab6076af08d806f460c7f73d05
Sjors Scholten <s.scholten@student.fhict.nl>
2018-05-24 12:28:37 +0200

    start button and exit button works

--- Assets/Scripts/MainMenu/SceneSwitcher.cs
+++ Assets/Scripts/MainMenu/SceneSwitcher.cs
@@ -4,11 +4,14 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
+[System.Serializable]
 public class SceneSwitcher : MonoBehaviour {
     public string StartingGame = "";
+    public float buttonOffset = 20f;
     public Toggle radioButton;
     public ToggleGroup radioGroup;
-    public List<GameMode> Modes = new List<GameMode>();
+    public GameMode[] Modes;
+    private List<Toggle> radioButtons = new List<Toggle>();
 
     private void Start() {
         InitializeToggles();
@@ -16,8 +19,11 @@ public class SceneSwitcher : MonoBehaviour {
 
     private void InitializeToggles() {
         radioButton.group = radioGroup;
-        for (int g = 0; g < Modes.Count; g++) {
-            
+        for (int g = 0; g < Modes.Length; g++) {
+            RectTransform groupTransform = radioGroup.GetComponent<RectTransform>();
+            Vector3 position = new Vector3(groupTransform.position.x, groupTransform.position.y - buttonOffset * g);
+            Toggle t = Instantiate(radioButton, position, Quaternion.identity, radioGroup.transform);
+            radioButtons.Add(t);
         }
     }
 
@@ -26,6 +32,7 @@ public class SceneSwitcher : MonoBehaviour {
 
 }
 
+[System.Serializable]
 public struct GameMode {
     public string Name;
     public string SceneName;
