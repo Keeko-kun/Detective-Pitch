c0c8eb23ca7076bedacdaf6ec29a69b156943267
Gijs <g.vandenbiggelaar@gmail.com>
2018-05-24 11:00:21 +0200

    Face Detection Plugin

--- Assets/Scripts/ScoreStuff/PlayerEmotions.cs
+++ Assets/Scripts/ScoreStuff/PlayerEmotions.cs
@@ -2,64 +2,46 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
-//using Affdex;
+using Affdex;
 
-public class PlayerEmotions //: ImageResultsListener
+public class PlayerEmotions : ImageResultsListener
 {
 
     public float currentAnger;
-    public float currentDisgust;
-    public float currentSmile;
-    public float currentEyeClosure;
+    public float currentJoy;
     public float currentSadness;
-    public float currentBrowRaise;
-    public float currentChinRaise;
-    public float currentSurprise;
+    public float currentBrowFurrow;
+    public float chinRaise;
+    public float lipPress;
 
-    bool face;
-    public GUIStyle BoxTexture;
-	/*
+
     public override void onFaceFound(float timestamp, int faceId)
     {
-        face = true;
-        if (Debug.isDebugBuild) Debug.Log("Found the face!");
+        Debug.Log("Found the face!");
     }
 
     public override void onFaceLost(float timestamp, int faceId)
     {
-        currentDisgust = 0;
-        currentEyeClosure = 0;
         currentAnger = 0;
-        currentSmile = 0;
+        currentJoy = 0;
+        currentBrowFurrow = 0;
         currentSadness = 0;
-        currentBrowRaise = 0;
-        currentChinRaise = 0;
-        currentSurprise = 0;
-        face = false;
-        if (Debug.isDebugBuild) Debug.Log("Lost the face!");
+        chinRaise = 0;
+        lipPress = 0;
+        Debug.Log("Lost the face!");
     }
 
     public override void onImageResults(Dictionary<int, Face> faces)
     {
         if (faces.Count > 0)
         {
-            faces[0].Emotions.TryGetValue( Emotions.Disgust, out currentDisgust);
-            faces[0].Emotions.TryGetValue( Emotions.Sadness, out currentSadness);
+            faces[0].Emotions.TryGetValue(Emotions.Sadness, out currentSadness);
             faces[0].Emotions.TryGetValue(Emotions.Anger, out currentAnger);
-            faces[0].Emotions.TryGetValue(Emotions.Surprise, out currentSurprise);
-            faces[0].Expressions.TryGetValue( Expressions.EyeClosure, out currentEyeClosure);
-            faces[0].Expressions.TryGetValue( Expressions.Smile, out currentSmile);
-            faces[0].Expressions.TryGetValue(Expressions.BrowRaise, out currentBrowRaise);
-            faces[0].Expressions.TryGetValue(Expressions.ChinRaise, out currentChinRaise);
+            faces[0].Emotions.TryGetValue(Emotions.Joy, out currentJoy);
+            faces[0].Expressions.TryGetValue(Expressions.BrowFurrow, out currentBrowFurrow);
+            faces[0].Expressions.TryGetValue(Expressions.ChinRaise, out chinRaise);
+            faces[0].Expressions.TryGetValue(Expressions.LipPress, out lipPress);
         }
     }
-    */
 
-    void OnGUI()
-    {
-        if (!face)
-        {
-            GUI.Box(new Rect(0, 0, Screen.width, Screen.height), "Looking for a face...", BoxTexture);
-        }
-    }
 }
--- Assets/Scripts/ScoreStuff/ScoreManager.cs
+++ Assets/Scripts/ScoreStuff/ScoreManager.cs
@@ -1,63 +1,63 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿//using System.Collections;
+//using System.Collections.Generic;
+//using UnityEngine;
 
-public class ScoreManager : MonoBehaviour {
+//public class ScoreManager : MonoBehaviour {
 
-    int score;
-    int desiredEmotion;
-    Transform player;
-    PlayerEmotions playerEmotions;
+//    int score;
+//    int desiredEmotion;
+//    Transform player;
+//    PlayerEmotions playerEmotions;
 
-    // Use this for initialization
-    void Start () {
+//    // Use this for initialization
+//    void Start () {
 		
-	}
+//	}
 	
-	// Update is called once per frame
-	void Update () {
+//	// Update is called once per frame
+//	void Update () {
 		
-	}
+//	}
 
-    void GetFace(PlayerEmotions currentEmotion)
-    {
-        player = GameObject.FindGameObjectWithTag("Player").transform;
-        playerEmotions = player.GetComponent<PlayerEmotions>();
-        float dominantEmotion = Mathf.Max(playerEmotions.currentAnger, playerEmotions.currentDisgust, playerEmotions.currentEyeClosure,
-                                       playerEmotions.currentSmile, playerEmotions.currentSurprise, playerEmotions.currentBrowRaise,
-                                       playerEmotions.currentChinRaise, playerEmotions.currentSadness);
+//    void GetFace(PlayerEmotions currentEmotion)
+//    {
+//        player = GameObject.FindGameObjectWithTag("Player").transform;
+//        playerEmotions = player.GetComponent<PlayerEmotions>();
+//        float dominantEmotion = Mathf.Max(playerEmotions.currentAnger, playerEmotions.currentDisgust, playerEmotions.currentEyeClosure,
+//                                       playerEmotions.currentSmile, playerEmotions.currentSurprise, playerEmotions.currentBrowRaise,
+//                                       playerEmotions.currentChinRaise, playerEmotions.currentSadness);
 
-        if (playerEmotions.currentAnger == dominantEmotion && desiredEmotion == 1)
-        {
-            score++;
-        }
-        else if (playerEmotions.currentDisgust == dominantEmotion && desiredEmotion == 2)
-        {
-            score++;
-        }
-        else if (playerEmotions.currentEyeClosure == dominantEmotion && desiredEmotion == 3)
-        {
-            score++;
-        }
-        else if (playerEmotions.currentSmile == dominantEmotion && desiredEmotion == 4)
-        {
-            score++;
-        }
-        else if (playerEmotions.currentSurprise == dominantEmotion && desiredEmotion == 5)
-        {
-            score++;
-        }
-        else if (playerEmotions.currentBrowRaise == dominantEmotion && desiredEmotion == 6)
-        {
-            score++;
-        }
-        else if (playerEmotions.currentChinRaise == dominantEmotion && desiredEmotion == 7)
-        {
-            score++;
-        }
-        else if (playerEmotions.currentSadness == dominantEmotion && desiredEmotion == 8)
-        {
-            score++;
-        }
-    }
-}
+//        if (playerEmotions.currentAnger == dominantEmotion && desiredEmotion == 1)
+//        {
+//            score++;
+//        }
+//        else if (playerEmotions.currentDisgust == dominantEmotion && desiredEmotion == 2)
+//        {
+//            score++;
+//        }
+//        else if (playerEmotions.currentEyeClosure == dominantEmotion && desiredEmotion == 3)
+//        {
+//            score++;
+//        }
+//        else if (playerEmotions.currentSmile == dominantEmotion && desiredEmotion == 4)
+//        {
+//            score++;
+//        }
+//        else if (playerEmotions.currentSurprise == dominantEmotion && desiredEmotion == 5)
+//        {
+//            score++;
+//        }
+//        else if (playerEmotions.currentBrowRaise == dominantEmotion && desiredEmotion == 6)
+//        {
+//            score++;
+//        }
+//        else if (playerEmotions.currentChinRaise == dominantEmotion && desiredEmotion == 7)
+//        {
+//            score++;
+//        }
+//        else if (playerEmotions.currentSadness == dominantEmotion && desiredEmotion == 8)
+//        {
+//            score++;
+//        }
+//    }
+//}
