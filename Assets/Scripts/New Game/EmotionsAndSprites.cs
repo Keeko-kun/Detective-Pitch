57d8adbe1dfaad88616189b9adc5de3d1b412cb3
NinjaCracker <o.olaf@hotmail.com>
2018-06-08 11:11:32 +0200

    Hallway emotion
    Each hallway has it's own emotion

---
+++ Assets/Scripts/New Game/EmotionsAndSprites.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "New Emotions", menuName = "Emotions")]
+public class EmotionsAndSprites : ScriptableObject {
+
+	public List<ImageEnum> emotions;
+}
