7b5bb3b43d6a5c0c9a8a51329d7d8e872edecd9f
Boekel,Kelvin K. van <kelvinvanboekel@gmail.com>
2018-06-08 13:14:30 +0200

    merging

--- Assets/Scripts/New Game/SpawnCorridor.cs
+++ Assets/Scripts/New Game/SpawnCorridor.cs
@@ -1,23 +1,33 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Affdex;
 
 public class SpawnCorridor : MonoBehaviour {
 
-	public GameObject corridor;
+	public GameObject prefab;
+	public EmotionsAndSprites spriteEmotion;
+	public List<Emotions> emotionsOfCorridors;
 	public int numberOfCorridors;
 	public float zOffset;
 	private Vector3 position;
 
 	// Use this for initialization
 	void Start () {
+		emotionsOfCorridors = new List<Emotions> ();
 		while (numberOfCorridors > 0)
 		{
 			if(position == null)
 			{
 				position = Vector3.zero;
 			}
-			Instantiate (corridor, position, corridor.transform.rotation);
+			GameObject corridor = Instantiate (prefab, position, prefab.transform.rotation, gameObject.transform) as GameObject;
+
+			HallwayEmotion hallwayScript = corridor.GetComponent<HallwayEmotion> ();
+			ImageEnum imageEnum = (ImageEnum) spriteEmotion.emotions.ToArray().GetValue(Random.Range (0, spriteEmotion.emotions.Count));
+			hallwayScript.ChangeSprite(imageEnum.image);
+			emotionsOfCorridors.Add (imageEnum.emotion);
+
 			position.z += zOffset;
 			numberOfCorridors--;
 		}
